9bccf2e6f9cb8b69aff45056eac7a12b7fc2cb70
Majuth <majuth101@hotmail.com>
2023-12-19 22:39:16 -0500

    Add Menu Content

---
+++ data.ts
@@ -0,0 +1,26 @@
+export const MENU = [
+    {
+      name: "Home",
+      ref: "home",
+    },
+    {
+      name: "Projects",
+      ref: "projects",
+    },
+    {
+      name: "Education",
+      ref: "education",
+    },
+    {
+      name: "Skills",
+      ref: "skills",
+    },
+    {
+      name: "Experience",
+      ref: "experience",
+    },
+    {
+      name: "Contact",
+      ref: "contact",
+    },
+  ];
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
+
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
     <header className="w-full fixed top-0 py-8 select-none z-40 bg-gradient-to-b from-slate-950 to-transparent">
         <div className="flex justify-between section-container">
-        <a href="#home" className="link">
-            <img src="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>👨‍💻</text></svg>" width={25} height={25} />
-        </a>
+            <a href="#home" className="link">
+                <img src="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>👨‍💻</text></svg>" width={25} height={25} alt="Logo - Majuth"/>
+            </a>
+            <nav className={` ${isMenuOpen ? "menu-visible" : ""}`}>
+            <button 
+                className="w-6 h-6 flex items-center justify-center link relative"
+                onClick={setIsMenuOpen.bind(null, !isMenuOpen)}    
+            >
+                <div className="relative flex-none w-full bg-white duration-300 flex items-center justify-center"></div>
+            </button>
+            </nav>
         </div>
-        <nav>
-          <button className="hamburger w-6 h-6 flex items-center justify-center link relative">
-          </button>
-        </nav>
     </header>
     );
     
