84f84ea0462eb99409ec4550281bfec4f56f2398
Majuth <majuth101@hotmail.com>
2024-01-26 21:29:07 -0500

    Change to blue theme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-    <header className="w-full fixed top-0 py-8 select-none z-40 bg-gradient-to-b from-slate-950 to-transparent">
+    <header className="w-full fixed top-0 py-8 select-none z-40 bg-gradient-to-b from-gray-900 to-transparent">
         <div className="flex justify-between section-container">
             <a href="#home" className="link">
                 <img src="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>👨‍💻</text></svg>" width={25} height={25} alt="Logo - Majuth"/>
