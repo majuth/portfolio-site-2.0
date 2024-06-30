cffdfda62d006a573d3d171a4418b3ebdad82103
Majuth <majuth101@hotmail.com>
2024-06-30 03:04:06 -0400

    Add scroll animation and temp spacing for it

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,10 +1,41 @@
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import { gsap, Linear, ScrollTrigger } from "gsap/all";
 
 function Project() {
     const sectionTitleRef = React.useRef(null);
     const targetSection = React.useRef(null);
 
     const [willChange, setwillChange] = useState(false);
+
+    function initRevealAnimation(targetSection){
+        const revealTimeline = gsap.timeline({
+            defaults: {ease: Linear.easeNone}
+        });
+
+        revealTimeline.fromTo(
+            targetSection.current.querySelectorAll(".seq"),
+            { opacity: 0 },
+            { opacity: 1, duration: 0.5, stagger: 0.5 },
+            "<"
+        );
+
+        const ScrollTriggerInstance = ScrollTrigger.create({
+            trigger: targetSection.current,
+            start: "top bottom",
+            end: "bottom bottom",
+            scrub: 0,
+            animation: revealTimeline,
+        });
+
+        return [revealTimeline, ScrollTriggerInstance];
+    }
+
+    useEffect(() =>{
+        const [revealTimelineInstance, initRevealTriggerInstance] = initRevealAnimation(targetSection);
+
+        return initRevealTriggerInstance.kill;
+    }, [targetSection]);
+
 
     function renderProjectTitle(){
         return <div
@@ -18,12 +49,22 @@ function Project() {
             <h2 className="text-2xl md:max-w-3xl w-full seq max-w-sm mt-2">
             I have contributed in over 10+ projects over various fields of development, including personal and school projects.
             </h2>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
         </div>
     }
 
     return <section
     ref={targetSection}
     className="w-full relative select-none section-container flex-col flex py-8 justify-center"
+    id={"projects"}
     >
         {renderProjectTitle()}
     </section>
