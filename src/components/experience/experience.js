02781e57ac6cce60a590227f5f89cb419f23dfde
Majuth <majuth101@hotmail.com>
2024-08-16 02:24:21 -0400

    progress on svg line

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -11,6 +11,58 @@ function Experience() {
     const curveLength = 150;
     const dotSize = 26;
 
+    const [svgWidth, setSvgWidth] = useState(400);
+
+    const svgCheckpointItems = TIMELINE.filter(
+        (item) => item.shouldDrawLine === true
+      );
+    const svgLength = svgCheckpointItems?.length * separation;
+    const timelineSvg = React.useRef(null);
+    const svgContainer = React.useRef(null);
+
+    function addNodeRefsToItems(timeline){
+        return timeline.map((node, idx) => ({
+            ...node,
+            next: timeline[idx + 1],
+            prev: timeline[idx - 1],
+          }));
+    }
+
+    function generateTimelineSvg(timeline){
+        let index = 1;
+        let y = dotSize / 2;
+        const timelineStyle = `<style>.str, .dot{stroke-width: ${strokeWidth}px}.anim-branch{stroke-dasharray: 186}</style>`;
+
+        timelineSvg = addNodeRefsToItems(timeline).reduce(
+            (svg, node) => {
+              const { type, next } = node;
+              let lineY = y;
+              let dotY = y + separation / 2;
+            },
+            timelineStyle
+          );
+      
+          return timelineSvg;
+    }
+
+    function getDotString(x, y){
+        return `<rect class='dot' width=${dotSize} height=${dotSize} fill='#111827' x=${
+            x - dotSize / 2
+          } y=${
+            y - dotSize / 2
+          } ></rect><circle cx=${x} cy=${y} r='7' stroke=${svgColor} class='dot' ></circle>`;
+    }
+
+    function renderSVG(){
+        return <svg
+        width={svgWidth}
+        height={svgLength}
+        viewBox={`0 0 ${svgWidth} ${svgLength}`}
+        fill="none"
+        ref={timelineSvg}
+      ></svg>
+    }
+    
     function renderSectionTitle(){
         return <div className="flex flex-col">
             <p className="section-title-sm seq">WORK</p>
@@ -26,7 +78,7 @@ function Experience() {
     >
         {renderSectionTitle()}
         <div className="gap-4 mt-20">
-            {/* {renderSVG()} */}
+            {renderSVG()}
         </div>
     </section>
 }
